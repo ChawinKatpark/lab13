f87e099429377d24814aa81bd306bae0b2ae6c6e
Chawin <chawin_katpark@cmu.ac.th>
2024-01-02 16:22:56 +0700

    lab13_1.completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -17,3 +17,47 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]) {
+    for (int i = 0; i < N; i++) {
+        B[0] += (A[i]/N);
+    }
+    
+    float sum = 0, standardDeviation = 0;
+  for(int i = 0; i < N; i++) {
+    sum += A[i];
+ }
+  
+  for(int i = 0; i < N; i++) {
+    standardDeviation += pow(A[i] - B[0], 2);
+  }
+    B[1] = sqrt(standardDeviation/N);
+    
+    float num = 1;
+    for (int i = 0; i < N; i++) {
+        num = num * A[i];
+    }
+    B[2] = pow(num,1.0/N);
+    
+    double sum2 = 0;
+    for (int i = 0; i < N; i++)
+        sum2 = sum2 + (1.0/A[i]);
+    B[3] = N/sum2;
+    
+    float high = 0;
+    for (int i = 0; i < N; i++) {
+        if (A[i] >= high) {
+            high = A[i];
+        }
+    }
+    B[4] = high;
+    
+    float low = B[4];
+    for (int i = 0; i < N; i++) {
+        if (A[i] < low) {
+            low = A[i];
+        }
+    }
+    B[5] = low;
+    
+}
